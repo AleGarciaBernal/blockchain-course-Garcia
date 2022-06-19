65d6b5e9958356aeda19436e8ffd61f13b12bdb7
AleGarciaBernal <aleandy7@gmail.com>
2022-06-19 17:50:50 -0400

    [15.06.2022] Deploy Ganache y Firebase corregido

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import './App.css';
 import {connectWallet, initialize} from "./ethereum/web3";
 import contractLottery from "./ethereum/abis/Lottery.json"
 import * as net from "net";
+
 function App() {
+
   const [contract, setContract] = useState<any>('');
+
   const [manager, setManager] = useState<any>('');
   const [players, setPlayers] = useState<any>([]);
   const [balance, setBalance] = useState<any>('');
+
   const [value, setValue] = useState<any>('');
   const [message, setMessage] = useState<any>('');
+
   useEffect( ()=> {
     // @ts-ignore
     if(window.web3) {
@@ -18,37 +23,48 @@ function App() {
       loadBlockchainData()
     }
   }, [])
+
   const loadBlockchainData = async () => {
+
     //const contractDeployed;
+
     // @ts-ignore
     const Web3 = window.web3;
+
     // Rinkeby 4, Ganache 5777, BSC 97
-    const networkData = contract.networks['5777'];
+    const networkData = contractLottery.networks['5777'];
     console.log('networkData:', networkData);
+
     if(networkData) {
       const abi = contractLottery.abi;
       const address = networkData.address;
       console.log('address: ', address);
       const contractDeployed = new Web3.eth.Contract(abi, address);
+
       const players = await contractDeployed.methods.getPlayers().call();
       setPlayers(players);
       const manager = await contractDeployed.methods.manager().call();
       setManager(manager)
       const balance = await Web3.eth.getBalance(contractDeployed.options.address)
       setBalance(balance)
+
       setContract(contractDeployed)
     }
+
   }
+
   const loadBalance = async () => {
     // @ts-ignore
     const Web3 = window.web3;
     const balance = await Web3.eth.getBalance(contract.options.address)
     setBalance(balance)
   }
+
   const loadPlayers = async () => {
     const players = await contract.methods.getPlayers().call();
     setPlayers(players);
   }
+
   const onEnter = async () => {
     // @ts-ignore
     const Web3 = window.web3;
@@ -62,18 +78,22 @@ function App() {
     loadBalance();
     loadPlayers();
   }
+
   const onPickWinner = async () => {
     // @ts-ignore
     const Web3 = window.web3;
     const accounts = await Web3.eth.getAccounts()
     setMessage("waiting on transaction success...")
+
     await contract.methods.pickWinner().send({
       from: accounts[0]
     })
+
     setMessage("A winner has been picked!")
     loadBalance();
     loadPlayers();
   }
+
   return (
       <div className="App">
         <header className="App-header">
@@ -83,19 +103,21 @@ function App() {
           </p>
           <p>Hi React, Truffle, Firebase</p>
           <button onClick={() => connectWallet()} className="btn btn-success">Connect</button>
+
           <button onClick={ () => onPickWinner() } className="btn btn-success">Pick Winner</button>
+
           <p>PLAYERS: {players.length}</p>
           <p>BALANCE: {balance}</p>
           <p>MANAGER: {manager}</p>
+
           <p>Monto minimo mayor a 2 ETH</p>
           <input type="text" value={value} onChange={ (event) => { setValue(event.target.value) } }/>
           <button onClick={ () => { onEnter() } } className="btn btn-warning">Enter</button>
           <p>{message}</p>
+
         </header>
       </div>
   );
-
-
 }
-//bootstrap
+
 export default App;
