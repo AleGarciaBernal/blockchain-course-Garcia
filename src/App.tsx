b215c591721acc7e33a012d7f9affc4f1eab8796
AleGarciaBernal <aleandy7@gmail.com>
2022-06-18 23:16:32 -0400

    [13.06.2022] Task 2 get Manager, Get Balance

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { connectWallet, initialize } from './ethereum/web3';
+import {connectWallet, initialize} from "./ethereum/web3";
 import contractLottery from "./ethereum/abis/Lottery.json"
+import * as net from "net";
 
 function App() {
 
-  const [contract,setContract]= useState<any>('')
-  const [manager,setManager]= useState<any>('')
-  const [players,setPlayers]= useState<any>([])
-  const [balance,setBalance]= useState<any>('')
+  const [contract, setContract] = useState<any>('');
+
+  const [manager, setManager] = useState<any>('');
+  const [players, setPlayers] = useState<any>([]);
+  const [balance, setBalance] = useState<any>('');
+  //console.log(contract)
+  //setContract('nuevo valor')
 
 
-  useEffect(() => {
-    //@ts-ignore
+  /*componentDidMount(){
+  }*/
+
+  useEffect( ()=> {
+    // @ts-ignore
     if(window.web3) {
       initialize()
-      loadBlockChainData()
+      loadBlockchainData()
     }
-  },[])
+  }, [])
 
-  const loadBlockChainData = async ()=> {
-    //@ts-ignore
-    const Web3 = window.web3
-    const networkData = contractLottery.networks['5777']
-    console.log("Network Data: ",networkData)
+  const loadBlockchainData = async () => {
 
-    if(networkData){
-      const abi = contractLottery.abi
-      const address = networkData.address
-      console.log('address: ', address)
-      const contractDeployed = new Web3.eth.Contract(abi,address)
-      console.log(contractDeployed.methods.getPlayers().call())
+    //const contractDeployed;
 
-      const players = await contractDeployed.methods.getPlayers().call();
-      setPlayers(players)
+    // @ts-ignore
+    const Web3 = window.web3;
 
-      const manager = await contractDeployed.methods.manager().call()
+    // Rinkeby 4, Ganache 5777, BSC 97
+    const networkData = contractLottery.networks['5777'];
+    console.log('networkData:', networkData);
+
+    if(networkData) {
+      const abi = contractLottery.abi;
+      const address = networkData.address;
+      console.log('address: ', address);
+      const contractDeployed = new Web3.eth.Contract(abi, address);
+      await setContract(contractDeployed)
+
+      const players = await contract.methods.getPlayers().call();
+      setPlayers(players);
+      const manager = await contract.methods.manager().call();
       setManager(manager)
-
-      const balance = await Web3.eth.getBalance(contractDeployed.options.address)
+      const balance = await Web3.eth.getBalance(contract.options.address)
       setBalance(balance)
-
-      setContract(contractDeployed)
     }
+
   }
 
   return (
@@ -51,24 +60,17 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Truffle, Firebase, React
+            Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <button onClick={()=>connectWallet()}>Connect</button>
-          <p>Players: {players.length}</p>
-          <p>Balance: {balance}</p>
-          <p>Manager: {manager}</p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Truffle, Firebase, React
-          </a>
+          <p>Hi React, Truffle, Firebase</p>
+          <button onClick={() => connectWallet()}>Connect</button>
+
+          <p>PLAYERS: {players.length}</p>
+          <p>BALANCE: {balance}</p>
+          <p>MANAGER: {manager}</p>
         </header>
       </div>
   );
-
 
 
 }
