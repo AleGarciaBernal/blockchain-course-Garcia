060a5d8e38073fceab9a4280f3360df73fbbe39d
AleGarciaBernal <aleandy7@gmail.com>
2022-06-19 12:32:57 -0400

    [14.06.2022] Task 2 OnPickWinner implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,101 @@
-
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { connectWallet, initialize } from './ethereum/web3';
+import {connectWallet, initialize} from "./ethereum/web3";
 import contractLottery from "./ethereum/abis/Lottery.json"
+import * as net from "net";
 function App() {
-  const [contract,setContract]= useState<any>('')
-  const [manager,setManager]= useState<any>('')
-  const [players,setPlayers]= useState<any>([])
-  const [balance,setBalance]= useState<any>('')
-  const [value,setValue] = useState<any>('')
-  const [message, setMessage] = useState<any>('')
-
-
-  useEffect(() => {
-    //@ts-ignore
+  const [contract, setContract] = useState<any>('');
+  const [manager, setManager] = useState<any>('');
+  const [players, setPlayers] = useState<any>([]);
+  const [balance, setBalance] = useState<any>('');
+  const [value, setValue] = useState<any>('');
+  const [message, setMessage] = useState<any>('');
+  useEffect( ()=> {
+    // @ts-ignore
     if(window.web3) {
       initialize()
-      loadBlockChainData()
+      loadBlockchainData()
     }
-  },[])
-  const loadBlockChainData = async ()=> {
-    //@ts-ignore
-    const Web3 = window.web3
-    const networkData = contractLottery.networks['5777']
-    console.log("Network Data: ",networkData)
-    if(networkData){
-      const abi = contractLottery.abi
-      const address = networkData.address
-      console.log('address: ', address)
-      const contractDeployed = new Web3.eth.Contract(abi,address)
-      console.log(contractDeployed.methods.getPlayers().call())
-
+  }, [])
+  const loadBlockchainData = async () => {
+    //const contractDeployed;
+    // @ts-ignore
+    const Web3 = window.web3;
+    // Rinkeby 4, Ganache 5777, BSC 97
+    const networkData = contractLottery.networks['5777'];
+    console.log('networkData:', networkData);
+    if(networkData) {
+      const abi = contractLottery.abi;
+      const address = networkData.address;
+      console.log('address: ', address);
+      const contractDeployed = new Web3.eth.Contract(abi, address);
       const players = await contractDeployed.methods.getPlayers().call();
-      setPlayers(players)
-      const manager = await contractDeployed.methods.manager().call()
+      setPlayers(players);
+      const manager = await contractDeployed.methods.manager().call();
       setManager(manager)
       const balance = await Web3.eth.getBalance(contractDeployed.options.address)
       setBalance(balance)
       setContract(contractDeployed)
     }
   }
-
-  const onEnter = async () =>{
-    //@ts-ignore
-    const Web3 = window.web3
-    const accounts = await Web3.eth.getAccounts()
-
-    setMessage("waiting on transaction success...")
-
-    await contract.methods.enter().send({
-      from:accounts[0],
-      value:Web3.utils.toWei(value,"ether")
-    })
-
-    setMessage("You've entered the game...")
+  const loadBalance = async () => {
+    // @ts-ignore
+    const Web3 = window.web3;
+    const balance = await Web3.eth.getBalance(contract.options.address)
+    setBalance(balance)
   }
-
+  const loadPlayers = async () => {
+    const players = await contract.methods.getPlayers().call();
+    setPlayers(players);
+  }
+  const onEnter = async () => {
+    // @ts-ignore
+    const Web3 = window.web3;
+    const accounts = await Web3.eth.getAccounts()
+    setMessage("waiting on transaction success...")
+    await contract.methods.enter().send({
+      from: accounts[0],
+      value: Web3.utils.toWei(value, "ether")
+    })
+    setMessage("you have been entered...")
+    loadBalance();
+    loadPlayers();
+  }
+  const onPickWinner = async () => {
+    // @ts-ignore
+    const Web3 = window.web3;
+    const accounts = await Web3.eth.getAccounts()
+    setMessage("waiting on transaction success...")
+    await contract.methods.pickWinner().send({
+      from: accounts[0]
+    })
+    setMessage("A winner has been picked!")
+    loadBalance();
+    loadPlayers();
+  }
   return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Truffle, Firebase, React
+            Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <button onClick={()=>connectWallet()}>Connect</button>
-          <p>Players: {players.length}</p>
-          <p>Balance: {balance}</p>
-          <p>Manager: {manager}</p>
-          <p>Monto Minimo 2 ETH</p>
-          <input type="text" value={value} onChange={ (event) => {setValue(event.target.value)}}></input>
-          <button onClick={ () => {onEnter()}}>Enter</button>
+          <p>Hi React, Truffle, Firebase</p>
+          <button onClick={() => connectWallet()} className="btn btn-success">Connect</button>
+          <button onClick={ () => onPickWinner() } className="btn btn-success">Pick Winner</button>
+          <p>PLAYERS: {players.length}</p>
+          <p>BALANCE: {balance}</p>
+          <p>MANAGER: {manager}</p>
+          <p>Monto minimo mayor a 2 ETH</p>
+          <input type="text" value={value} onChange={ (event) => { setValue(event.target.value) } }/>
+          <button onClick={ () => { onEnter() } } className="btn btn-warning">Enter</button>
           <p>{message}</p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Truffle, Firebase, React
-          </a>
         </header>
       </div>
   );
+
+
 }
+
 export default App;
